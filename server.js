bbe2fcdbd853c55feaa2f85c8108cc5549aa7926
dev-aditya99 <mail.adityasharma99@gmail.com>
2025-11-15 00:19:27 +0530

    Make able to use the backend

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 const server = http.createServer(app);
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173", // The frontend domain
+    origin: "*", // The frontend domain
     credentials: true, // This allows cookies to be sent in cross-origin requests
     methods: ["POST", "GET"]
 }));
